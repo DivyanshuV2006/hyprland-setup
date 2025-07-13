47ff4dd320e6c3dd837302b14f7b5733f0ece14b
Divyanshu Verma <79598448+DivyanshuV2006@users.noreply.github.com>
2025-07-13 15:56:02 -0500

    Create install-hyprland.sh

---
+++ install-hyprland.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo pacman -S --noconfirm \
+  xdg-desktop-portal-hyprland \
+  wl-clipboard \
+  waybar \
+  kitty \
+  wofi \
+  polkit-gnome \
+  pipewire \
+  wireplumber \
+  pipewire-audio \
+  pipewire-pulse \
+  pipewire-alsa \
+  bluez bluez-utils \
+  network-manager-applet \
+  grim slurp \
+  swaybg \
+  sddm \
+  qt5-wayland qt6-wayland \
+  xdg-utils \
+  firefox
